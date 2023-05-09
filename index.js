cc5e472deeea8e600f8955a9760c55bb96d95f29
Alcade <alcade.benjamin@gmail.com>
2023-05-09 05:48:04 +0200

    add bubble

---
+++ index.js
@@ -0,0 +1,28 @@
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  bubble.style.setProperty("--left", Math.random() * 100 + "%");
+
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 400);
